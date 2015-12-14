9376aefc37538dc2bec8be00e536150e0b03edb3
Sebastian Weigelt <weigelt@kit.edu>
2015-12-14 14:32:25 +0100

    add graph creation and print to test

--- src/test/java/edu/kit/ipd/parse/shallownlp/IntegrationTest.java
+++ src/test/java/edu/kit/ipd/parse/shallownlp/IntegrationTest.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import edu.kit.ipd.parse.luna.graph.GraphInterface;
+
 public class IntegrationTest {
 	@Test
 	public void showSingleStringOutput() {
@@ -11,6 +13,8 @@ public class IntegrationTest {
 		ShallowNLP snlp = new ShallowNLP();
 		Token[] actual = snlp.parse(input, false, true, true, null);
 		System.out.println(Arrays.deepToString(actual));
+		GraphInterface graph = snlp.createAGGGraph(actual);
+		graph.showGraph();
 	}
 
 }
