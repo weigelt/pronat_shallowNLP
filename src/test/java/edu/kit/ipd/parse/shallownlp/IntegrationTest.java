588c46c97a0c055417868f5225874e4adde84f70
Sebastian Weigelt <weigelt@kit.edu>
2016-01-03 13:06:40 +0100

    defined variables as instance variables as tests are disjunct, add a test that uses snlp as pipeline stage

--- src/test/java/edu/kit/ipd/parse/shallownlp/IntegrationTest.java
+++ src/test/java/edu/kit/ipd/parse/shallownlp/IntegrationTest.java
@@ -2,19 +2,46 @@ package edu.kit.ipd.parse.shallownlp;
 
 import java.util.Arrays;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import edu.kit.ipd.parse.luna.data.MissingDataException;
+import edu.kit.ipd.parse.luna.data.PrePipelineData;
 import edu.kit.ipd.parse.luna.graph.GraphInterface;
 
 public class IntegrationTest {
+
+	ShallowNLP snlp;
+	String input;
+	Token[] actual;
+	GraphInterface graph;
+
+	@Before
+	public void setUp() {
+		snlp = new ShallowNLP();
+	}
+
 	@Test
 	public void showSingleStringOutput() {
-		String input = "Armar go to the fridge";
-		ShallowNLP snlp = new ShallowNLP();
-		Token[] actual = snlp.parse(input, false, true, true, null);
+		input = "Armar go to the fridge";
+		actual = snlp.parse(input, false, true, true, null);
 		System.out.println(Arrays.deepToString(actual));
-		GraphInterface graph = snlp.createAGGGraph(actual);
+		graph = snlp.createAGGGraph(actual);
 		graph.showGraph();
+	}
+
+	@Test
+	public void pipelineStageTest() {
+		PrePipelineData ppd = new PrePipelineData();
+		ppd.setTranscription("Armar go to the fridge");
+		snlp.exec(ppd);
+		try {
+			graph = ppd.getGraph();
+		} catch (MissingDataException e) {
+			e.printStackTrace();
+		}
+		graph.showGraph();
+
 	}
 
 }
