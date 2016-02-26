e02d94dd83737ffd2a625b576e5293b069a820aa
Sebastian Weigelt <weigelt@kit.edu>
2016-02-26 12:59:43 +0100

    add IllegalArgument exception to calcInstr

--- src/main/java/edu/kit/ipd/parse/shallownlp/CalcInstruction.java
+++ src/main/java/edu/kit/ipd/parse/shallownlp/CalcInstruction.java
@@ -1,36 +1,44 @@
 package edu.kit.ipd.parse.shallownlp;
 
 /**
- * This class contains the heuristic to calculate the instruction number for each word
+ * This class contains the heuristic to calculate the instruction number for
+ * each word
+ * 
  * @author Markus Kocybik
  */
 public class CalcInstruction {
 
 	/**
-	 * This method calculates the instruction number for each word of the input text.
-	 * @param words each element represents one word of the input text 
-	 * @param pos the pos tags for each word
+	 * This method calculates the instruction number for each word of the input
+	 * text.
+	 * 
+	 * @param words
+	 *            each element represents one word of the input text
+	 * @param pos
+	 *            the pos tags for each word
 	 * @return the instruction number for each word
+	 * 
+	 * @throws IllegalArgumentException
+	 *             throws an exception if word aaray and pos array have
+	 *             different lengths
 	 */
-	public int[] CalculateInstructionNumber(String[] words, String[] pos) {
+	public int[] CalculateInstructionNumber(String[] words, String[] pos) throws IllegalArgumentException {
 		if (words.length == pos.length) {
 			int[] list = new int[words.length];
 			int instrNr = 0;
-			int verbCounter = 0; 
+			int verbCounter = 0;
 			for (int i = 0; i < words.length; i++) {
 				// looking for a verb
-				if (verbCounter == 0 ) {
+				if (verbCounter == 0) {
 					//special case: two verbs in a row
-					if (pos[i].startsWith("VB") 
-							&& i < words.length - 1 
-							&& pos[i+1].startsWith("VB")) {
+					if (pos[i].startsWith("VB") && i < words.length - 1 && pos[i + 1].startsWith("VB")) {
 						list[i] = instrNr;
-						list[i+1] = instrNr;
+						list[i + 1] = instrNr;
 						i++;
 						verbCounter++;
 					}
 					//verb found
-					else if (pos[i].startsWith("VB")){
+					else if (pos[i].startsWith("VB")) {
 						list[i] = instrNr;
 						verbCounter++;
 					}
@@ -42,9 +50,8 @@ public class CalcInstruction {
 				//looking for an instruction boundary
 				else {
 					// these words represent instruction boundaries
-					if (words[i].toLowerCase().equals("and") 
-						|| words[i].toLowerCase().equals("if") 
-						|| words[i].toLowerCase().equals("when")) {
+					if (words[i].toLowerCase().equals("and") || words[i].toLowerCase().equals("if")
+							|| words[i].toLowerCase().equals("when")) {
 						verbCounter = 0;
 						instrNr++;
 						list[i] = instrNr;
@@ -54,16 +61,14 @@ public class CalcInstruction {
 						instrNr++;
 						verbCounter = 0;
 						i--; //repeat loop
-					}
-					else {
+					} else {
 						list[i] = instrNr;
 					}
 				}
 			}
 			return list;
-		}
-		else {
-			return null;
+		} else {
+			throw new IllegalArgumentException("word array and pos array have different lengths");
 		}
 
 	}
