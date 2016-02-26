f36fb6e2ed01e524469f9b5a7c9f5d6dff5c5700
Sebastian Weigelt <weigelt@kit.edu>
2016-02-26 13:46:14 +0100

    Model is now in config

--- src/main/java/edu/kit/ipd/parse/shallownlp/Stanford.java
+++ src/main/java/edu/kit/ipd/parse/shallownlp/Stanford.java
@@ -1,52 +1,64 @@
 package edu.kit.ipd.parse.shallownlp;
+
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
+import edu.kit.ipd.parse.luna.tools.ConfigManager;
+import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.ling.Sentence;
 import edu.stanford.nlp.ling.TaggedWord;
-import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.process.DocumentPreprocessor;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
 /**
  * This class represents a facade for Stanford CoreNLP
+ * 
  * @author Markus Kocybik
  */
-public class Stanford {	
-	
+public class Stanford {
+
+	Properties props;
+
+	Stanford() {
+		props = ConfigManager.getConfiguration(getClass());
+	}
+
 	/**
 	 * This method realizes pos tagging with the Stanford POS Tagger
-	 * @param text the input text. Each element in the array represents one word.
+	 * 
+	 * @param text
+	 *            the input text. Each element in the array represents one word.
 	 * @return the pos tags
 	 */
-	public String[] posTag(String[] text)
-	{
-		MaxentTagger tagger = 
-		new MaxentTagger("edu/stanford/nlp/models/pos-tagger/english-bidirectional/english-bidirectional-distsim.tagger");
+	public String[] posTag(String[] text) {
+		MaxentTagger tagger = new MaxentTagger(props.getProperty("SENNA_OPTIONS"));
 		List<HasWord> sent = Sentence.toWordList(text);
-	    List<TaggedWord> taggedSent = tagger.tagSentence(sent);
-	    String[] result = new String[taggedSent.size()];
-	    for (int i = 0; i < taggedSent.size(); i++) {
-	    	result[i] = taggedSent.get(i).tag();
-	    }
-	    return result;
+		List<TaggedWord> taggedSent = tagger.tagSentence(sent);
+		String[] result = new String[taggedSent.size()];
+		for (int i = 0; i < taggedSent.size(); i++) {
+			result[i] = taggedSent.get(i).tag();
+		}
+		return result;
 	}
-	
+
 	/**
 	 * This method splits a text which contains periods
-	 * @param text an input text with periods to split
+	 * 
+	 * @param text
+	 *            an input text with periods to split
 	 * @return each element in the array represents one sentence
 	 */
 	public String[] splitSentence(String text) {
-		DocumentPreprocessor dp = new DocumentPreprocessor( new StringReader(text));
+		DocumentPreprocessor dp = new DocumentPreprocessor(new StringReader(text));
 		List<String> sentenceList = new ArrayList<String>();
 
 		for (List<HasWord> sentence : dp) {
-		   String sentenceString = Sentence.listToString(sentence);
-		   sentenceList.add(sentenceString.toString());
+			String sentenceString = Sentence.listToString(sentence);
+			sentenceList.add(sentenceString.toString());
 		}
 		return sentenceList.toArray(new String[sentenceList.size()]);
 	}
-	
+
 }
