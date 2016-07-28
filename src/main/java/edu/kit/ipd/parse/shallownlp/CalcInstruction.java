317f875a1affdb972f67e37f482c283ef7c890fd
Tobias Hey <tobias.hey@student.kit.edu>
2016-07-28 15:52:41 +0200

    improved CalcInstruction with temporal keywords and slightly different heuristic

--- src/main/java/edu/kit/ipd/parse/shallownlp/CalcInstruction.java
+++ src/main/java/edu/kit/ipd/parse/shallownlp/CalcInstruction.java
@@ -5,6 +5,7 @@ package edu.kit.ipd.parse.shallownlp;
  * each word
  * 
  * @author Markus Kocybik
+ * @author Tobias Hey - extended Boundary Keywords for temporal Keywords (2016-07-28)
  */
 public class CalcInstruction {
 
@@ -28,48 +29,77 @@ public class CalcInstruction {
 			int instrNr = 0;
 			int verbCounter = 0;
 			for (int i = 0; i < words.length; i++) {
-				// looking for a verb
-				if (verbCounter == 0) {
-					//special case: two verbs in a row
-					if (pos[i].startsWith("VB") && i < words.length - 1 && pos[i + 1].startsWith("VB")) {
+				
+				
+				if (isInstructionBoundary(words[i])) {
+					// no verb in between boundaries resets instructionNumber and extends previous instruction
+					if (verbCounter == 0) {
+						resetLastInstruction(list, instrNr);
 						list[i] = instrNr;
-						list[i + 1] = instrNr;
-						i++;
-						verbCounter++;
-					}
-					//verb found
-					else if (pos[i].startsWith("VB")) {
-						list[i] = instrNr;
-						verbCounter++;
-					}
-					//no verb found
-					else {
-						list[i] = instrNr;
-					}
-				}
-				//looking for an instruction boundary
-				else {
-					// these words represent instruction boundaries
-					if (words[i].toLowerCase().equals("and") || words[i].toLowerCase().equals("if")
-							|| words[i].toLowerCase().equals("when")) {
-						verbCounter = 0;
-						instrNr++;
-						list[i] = instrNr;
-					}
-					//another verb also initiates a new instruction (imperative sentence)
-					else if (pos[i].startsWith("VB")) {
-						instrNr++;
-						verbCounter = 0;
-						i--; //repeat loop
+						
 					} else {
+						verbCounter = 0;
+						instrNr++;
 						list[i] = instrNr;
 					}
+				} else {
+					// search Verb
+					if (verbCounter == 0) {
+						//special case: two verbs in a row
+						if (pos[i].startsWith("VB") && i < words.length - 1 && pos[i + 1].startsWith("VB")) {
+							list[i] = instrNr;
+							list[i + 1] = instrNr;
+							i++;
+							verbCounter++;
+						}
+						//verb found
+						else if (pos[i].startsWith("VB")) {
+							list[i] = instrNr;
+							verbCounter++;
+						}
+						//no verb found
+						else {
+							list[i] = instrNr;
+						}
+					} else {
+						//another verb also initiates a new instruction (imperative sentence)
+						if (pos[i].startsWith("VB")) {
+							instrNr++;
+							verbCounter = 0;
+							i--; //repeat loop
+						} else {
+							list[i] = instrNr;
+						}
+					}
 				}
+				
+				
 			}
 			return list;
 		} else {
 			throw new IllegalArgumentException("word array and pos array have different lengths");
 		}
 
+	}
+	
+	private boolean isInstructionBoundary(String word) {
+		if (word.toLowerCase().equals("and") || word.toLowerCase().equals("if")
+				|| word.toLowerCase().equals("when") || word.toLowerCase().equals("then")) {
+			return true;
+		}
+		return false;
+	}
+	
+	private void resetLastInstruction(int[] list, int instrNum) {
+		int number = 0;
+		for (int i = 0; i < list.length; i++) {
+			if (list[i] > number && list[i] < instrNum) {
+				number = list[i];
+			}
+			if (list[i] == instrNum) {
+				list[i] = number;
+			}
+		}
+		
 	}
 }
