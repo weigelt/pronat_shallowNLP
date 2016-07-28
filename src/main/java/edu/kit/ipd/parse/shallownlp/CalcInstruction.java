c80d7dffd75688571f064c092c4ab303f5206113
Tobias Hey <tobias.hey@student.kit.edu>
2016-07-28 18:56:42 +0200

    added further temporal keywords for instruction boundaries

--- src/main/java/edu/kit/ipd/parse/shallownlp/CalcInstruction.java
+++ src/main/java/edu/kit/ipd/parse/shallownlp/CalcInstruction.java
@@ -1,13 +1,20 @@
 package edu.kit.ipd.parse.shallownlp;
 
+
+import java.util.Arrays;
+
 /**
  * This class contains the heuristic to calculate the instruction number for
  * each word
  * 
  * @author Markus Kocybik
- * @author Tobias Hey - extended Boundary Keywords for temporal Keywords (2016-07-28)
+ * @author Tobias Hey - extended Boundary Keywords for temporal Keywords
+ *         (2016-07-28)
  */
 public class CalcInstruction {
+
+	private static final String[] temporal_keywords = { "before", "after", "finally", "when", "afterwards", "then",
+			"later", "thereupon", "whereupon", "hereupon", "as", "previously" };
 
 	/**
 	 * This method calculates the instruction number for each word of the input
@@ -29,14 +36,14 @@ public class CalcInstruction {
 			int instrNr = 0;
 			int verbCounter = 0;
 			for (int i = 0; i < words.length; i++) {
-				
-				
+
 				if (isInstructionBoundary(words[i])) {
-					// no verb in between boundaries resets instructionNumber and extends previous instruction
+					// no verb in between boundaries resets instructionNumber
+					// and extends previous instruction
 					if (verbCounter == 0) {
 						resetLastInstruction(list, instrNr);
 						list[i] = instrNr;
-						
+
 					} else {
 						verbCounter = 0;
 						instrNr++;
@@ -45,35 +52,35 @@ public class CalcInstruction {
 				} else {
 					// search Verb
 					if (verbCounter == 0) {
-						//special case: two verbs in a row
+						// special case: two verbs in a row
 						if (pos[i].startsWith("VB") && i < words.length - 1 && pos[i + 1].startsWith("VB")) {
 							list[i] = instrNr;
 							list[i + 1] = instrNr;
 							i++;
 							verbCounter++;
 						}
-						//verb found
+						// verb found
 						else if (pos[i].startsWith("VB")) {
 							list[i] = instrNr;
 							verbCounter++;
 						}
-						//no verb found
+						// no verb found
 						else {
 							list[i] = instrNr;
 						}
 					} else {
-						//another verb also initiates a new instruction (imperative sentence)
+						// another verb also initiates a new instruction
+						// (imperative sentence)
 						if (pos[i].startsWith("VB")) {
 							instrNr++;
 							verbCounter = 0;
-							i--; //repeat loop
+							i--; // repeat loop
 						} else {
 							list[i] = instrNr;
 						}
 					}
 				}
-				
-				
+
 			}
 			return list;
 		} else {
@@ -81,15 +88,15 @@ public class CalcInstruction {
 		}
 
 	}
-	
+
 	private boolean isInstructionBoundary(String word) {
-		if (word.toLowerCase().equals("and") || word.toLowerCase().equals("if")
-				|| word.toLowerCase().equals("when") || word.toLowerCase().equals("then")) {
+		if (word.toLowerCase().equals("and") || word.toLowerCase().equals("if") || word.toLowerCase().equals("when")
+				|| Arrays.asList(temporal_keywords).contains(word)) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	private void resetLastInstruction(int[] list, int instrNum) {
 		int number = 0;
 		for (int i = 0; i < list.length; i++) {
@@ -100,6 +107,6 @@ public class CalcInstruction {
 				list[i] = number;
 			}
 		}
-		
+
 	}
 }
