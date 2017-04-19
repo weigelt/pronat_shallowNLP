f4ae5ede7d0d33e973339efe913dbbabc9ebbe9f
Tobias Hey <tobias.hey@kit.edu>
2017-04-19 17:16:21 +0200

    fixed bug causing condition check to fail

--- src/main/java/edu/kit/ipd/parse/brillRules/ConditionChecker.java
+++ src/main/java/edu/kit/ipd/parse/brillRules/ConditionChecker.java
@@ -6,6 +6,9 @@ package edu.kit.ipd.parse.brillRules;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Tobias Hey
  *
@@ -13,6 +16,8 @@ import java.util.List;
 public class ConditionChecker {
 
 	private static List<IConditionPart> partChecker;
+
+	private static final Logger logger = LoggerFactory.getLogger(ConditionChecker.class);
 
 	static {
 		partChecker = new ArrayList<>();
@@ -27,37 +32,53 @@ public class ConditionChecker {
 		private BooleanOperators(String op) {
 			this.op = op;
 		};
+
+		@Override
+		public String toString() {
+			return this.op;
+		}
 	}
 
 	public static boolean checkCondition(String condition, String[] words, String[] posTags, String[] chunks, int currIndex) {
 		boolean result = false;
 		String[] condParts = condition.trim().split(" ");
-		if (condParts.length != 0) {
-			String part = condParts[0];
-			if (part.equals(BooleanOperators.AND)) {
-				if (condParts.length > 1 && condParts[1].equals(BooleanOperators.NOT)) {
-					result = result && !checkCondition(buildCondition(condParts, 1), words, posTags, chunks, currIndex);
-				} else {
-					result = result && checkCondition(buildCondition(condParts, 0), words, posTags, chunks, currIndex);
-				}
-			} else if (part.equals(BooleanOperators.OR)) {
-				if (condParts.length > 1 && condParts[1].equals(BooleanOperators.NOT)) {
-					result = result || !checkCondition(buildCondition(condParts, 1), words, posTags, chunks, currIndex);
-				} else {
-					result = result || checkCondition(buildCondition(condParts, 0), words, posTags, chunks, currIndex);
-				}
-			} else if (part.equals(BooleanOperators.XOR)) {
-				if (condParts.length > 1 && condParts[1].equals(BooleanOperators.NOT)) {
-					result = result ^ !checkCondition(buildCondition(condParts, 1), words, posTags, chunks, currIndex);
-				} else {
-					result = result ^ checkCondition(buildCondition(condParts, 0), words, posTags, chunks, currIndex);
+		if (condParts.length != 0 && !condition.equals("")) {
+			int index = 1;
+			if (condParts.length > 1 && condParts[0].equalsIgnoreCase(BooleanOperators.NOT.toString())) {
+				for (IConditionPart pChecker : partChecker) {
+					if (pChecker.isPartOfType(condParts[1])) {
+						result = !pChecker.checkPart(condParts[1], words, posTags, chunks, currIndex);
+						index = 2;
+						break;
+					}
 				}
 			} else {
 				for (IConditionPart pChecker : partChecker) {
-					if (pChecker.isPartOfType(part)) {
-						result = pChecker.checkPart(part, words, posTags, chunks, currIndex);
+					if (pChecker.isPartOfType(condParts[0])) {
+						result = !pChecker.checkPart(condParts[0], words, posTags, chunks, currIndex);
+						index = 1;
+						break;
 					}
 				}
+			}
+			if (index < condParts.length) {
+				String part = condParts[index];
+				if (part.equalsIgnoreCase(BooleanOperators.AND.toString())) {
+
+					result = result && checkCondition(buildCondition(condParts, index), words, posTags, chunks, currIndex);
+
+				} else if (part.equalsIgnoreCase(BooleanOperators.OR.toString())) {
+
+					result = result || checkCondition(buildCondition(condParts, index), words, posTags, chunks, currIndex);
+
+				} else if (part.equalsIgnoreCase(BooleanOperators.XOR.toString())) {
+
+					result = result ^ checkCondition(buildCondition(condParts, index), words, posTags, chunks, currIndex);
+
+				} else {
+					logger.warn("Parts of the specified Condition are not combined by a boolean operator. Rule: " + condition);
+				}
+
 			}
 		} else {
 			return true;
