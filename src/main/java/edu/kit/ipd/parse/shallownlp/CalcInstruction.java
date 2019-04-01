4b7de5cebcc2c1983fa75365d0a4ca213140abe0
Sebastian Weigelt <weigelt@kit.edu>
2019-04-01 15:55:08 +0200

    implemented adv. heuristic for instr numbers

--- src/main/java/edu/kit/ipd/parse/shallownlp/CalcInstruction.java
+++ src/main/java/edu/kit/ipd/parse/shallownlp/CalcInstruction.java
@@ -31,16 +31,16 @@ public class CalcInstruction {
 	 * @return the instruction number for each word
 	 * 
 	 * @throws IllegalArgumentException
-	 *             throws an exception if word array and pos array have
-	 *             different lengths
+	 *             throws an exception if word array and pos array have different
+	 *             lengths
 	 */
-	int[] calculateInstructionNumber(String[] words, String[] pos, boolean legacyCalcInstrMode) throws IllegalArgumentException {
+	int[] calculateInstructionNumber(String[] words, String[] pos, String[] lemma, boolean legacyCalcInstrMode)
+			throws IllegalArgumentException {
 		if (words.length == pos.length) {
-			if (legacyCalcInstrMode){
+			if (legacyCalcInstrMode) {
 				return calculateInstructionNumberLegacy(words, pos);
-			}
-			else {
-				return calculateInstructionNumberAdvanced(words, pos);
+			} else {
+				return calculateInstructionNumberAdvanced(words, pos, lemma);
 			}
 		} else {
 			throw new IllegalArgumentException("word array and pos array have different lengths");
@@ -57,12 +57,61 @@ public class CalcInstruction {
 		return false;
 	}
 
-	private int[] calculateInstructionNumberAdvanced(String[] words, String[] pos){
-		// TODO: implement me!
-		return null;
+	private int[] calculateInstructionNumberAdvanced(String[] words, String[] pos, String[] lemma) {
+		int[] interInstTags = new int[words.length];
+		int[] resultInstTags = new int[words.length];
+		int currInst = 0;
+		boolean verbSeen = false, inVP = false, lastVerbVBG = false;
+
+		for (int i = 0; i < words.length; i++) {
+			interInstTags[i] = currInst;
+			if (isInstructionBoundary(words[i])) {
+				if (verbSeen) {
+					currInst++;
+					interInstTags[i] = currInst;
+					verbSeen = false;
+				}
+			} else {
+				if (pos[i].startsWith("VB")) {
+					verbSeen = true;
+					if (i != 0 && (lemma[i - 1].startsWith("have") || lemma[i - 1].startsWith("be")
+							|| (i > 1 && (pos[i - 1].startsWith("RB") && lemma[i - 2].startsWith("have")))
+							|| (i > 1 && (pos[i - 1].startsWith("RB") && lemma[i - 2].startsWith("be")))
+							|| (i > 1 && (pos[i - 1].startsWith("TO") && pos[i - 2].startsWith("VB")))
+							|| (i > 2 && (pos[i - 1].startsWith("RB") && pos[i - 2].startsWith("TO") && pos[i - 3].startsWith("VB"))))) {
+						inVP = true;
+					}
+					if (!inVP && !lastVerbVBG) {
+						currInst++;
+						interInstTags[i] = currInst;
+						if (i > 0 && pos[i - 1].startsWith("RB") && !(lemma[i - 1].startsWith("have") || lemma[i - 1].startsWith("be"))) {
+							interInstTags[i - 1] = currInst;
+						}
+					}
+					if (pos[i].startsWith("VBG")) {
+						lastVerbVBG = true;
+					}
+				}
+			}
+		}
+
+		currInst = 0;
+		verbSeen = false;
+
+		for (int i = 0; i < words.length; i++) {
+			if (i != 0 && verbSeen && interInstTags[i] != interInstTags[i - 1]) {
+				currInst++;
+				verbSeen = false;
+			}
+			resultInstTags[i] = currInst;
+			if (pos[i].startsWith("VB")) {
+				verbSeen = true;
+			}
+		}
+		return interInstTags;
 	}
 
-	private int[] calculateInstructionNumberLegacy(String[] words, String[] pos){
+	private int[] calculateInstructionNumberLegacy(String[] words, String[] pos) {
 		int[] list = new int[words.length];
 		int instrNr = 0;
 		int verbCounter = 0;
